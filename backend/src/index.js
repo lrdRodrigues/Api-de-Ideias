cff28df59afc99cfbdf8076836ac908db1ca7d6a
Rogerio Rodrigues <sp.rogerio@outlook.com>
2020-01-18 00:50:55 -0300

    Added Cors to the Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes/ideaRoutes')
+const cors = require('cors') 
 
 const USER = 'ideauser'
 const PASSWORD = 'ideapassword'
@@ -8,6 +9,7 @@ const PASSWORD = 'ideapassword'
 const URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0-b0doj.mongodb.net/tests?retryWrites=true&w=majority`
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
